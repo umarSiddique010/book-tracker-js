41c8a9d28b752e66b02d28ac4b7d802f86296f14
Md Umar Siddique <us70763@gamil.com>
2026-03-10 17:45:59 +0530

    build: add copy-webpack-plugin to serve public assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -28,6 +29,9 @@ export default {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/template.html'),
       favicon: path.resolve(__dirname, './src/asset/favicon.ico'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public', to: '.' }],
     }),
   ],
   module: {
